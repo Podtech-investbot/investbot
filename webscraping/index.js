886bcffd7c24e5048242ef5e10eefc42b328d0cc
Gleysson Rocha <gleyssonrocha@nucleus.eti.br>
2022-02-12 11:32:56 -0300

    [feature] Cria módulo para webscraping

---
+++ webscraping/index.js
@@ -0,0 +1,15 @@
+const puppeteer = require("puppeteer");
+
+module.exports = async function (codigo) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`https://investidor10.com.br/acoes/${codigo}/`);
+
+  let cotacao = await page.evaluate(() => {
+    return document.querySelector(".cotacao > ._card-body > div > span ")
+      .textContent;
+  });
+
+  await browser.close();
+  return cotacao;
+};
