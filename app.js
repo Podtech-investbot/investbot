736a1463e41852518586f9fb8e32ed853985d198
Gleysson Rocha <gleyssonrocha@nucleus.eti.br>
2022-02-12 11:33:28 -0300

    [feature] Busca cotação do ativo no comando /indicadores

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '5228161508:AAEDBP0EhlcSD6gqGy2DNXtT5OBw5yK_yAw';
+const TelegramBot = require("node-telegram-bot-api");
+const getCotacao = require("./webscraping/index");
+const token = "5228161508:AAEDBP0EhlcSD6gqGy2DNXtT5OBw5yK_yAw";
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Olá. Como vai?');
+  bot.sendMessage(chatId, "Olá. Como vai?");
 });
 
 bot.onText(/\/indicadores (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const stockCode = match[1];
-  bot.sendMessage(
-    chatId,
-    `Estamos buscando a informação para o ativo ${stockCode}`
-  );
+
+  bot.sendMessage(chatId, `Aguarde um instante...`);
+
+  getCotacao(stockCode)
+    .then(function (cotacao) {
+      bot.sendMessage(
+        chatId,
+        `A cotação atual para ${stockCode}  é: ${cotacao}`
+      );
+    })
+    .catch(function (err) {
+      bot.sendMessage(chatId, `Oppss, não encontramos o ativo! :/`);
+    });
 });
 
 bot.onText(/\/comparar (.+) (.+)/, (msg, match) => {
