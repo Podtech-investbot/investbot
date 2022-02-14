7ff0093e93d50fe6d7ab417952eb4e4cc2427f87
Gleysson Rocha <gleyssongaspar@gmail.com>
2022-02-14 10:03:53 -0300

    feat: Add webscraping to return share price by stock code (#4)
    * chore: Adiciona dependência puppeteer
    
    * [feature] Cria módulo para webscraping
    
    * [feature] Busca cotação do ativo no comando /indicadores
    
    * feat: Add puppeteer dependency
    
    * feat: Create webscraping module
    
    * feat: Call getSharePrice for command /indicadores
    
    * chore: Create .prettierrc file
    
    The prettierrc file tells your IDE how to format the code automatically according to lint rules.
    
    * lint: Remove unused var
    
    * chore: Add prettier dependency
    
    * lint: Add prettier dependency
    
    * lint: Change  prettier to dev dependencies
    
    Co-authored-by: Gleysson Rocha <gleyssonrocha@nucleus.eti.br>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const getSharePrice = require('./webscraping/index');
 const token = '5228161508:AAEDBP0EhlcSD6gqGy2DNXtT5OBw5yK_yAw';
 const bot = new TelegramBot(token, { polling: true });
 
@@ -10,10 +11,19 @@ bot.onText(/\/start/, (msg) => {
 bot.onText(/\/indicadores (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const stockCode = match[1];
-  bot.sendMessage(
-    chatId,
-    `Estamos buscando a informação para o ativo ${stockCode}`
-  );
+
+  bot.sendMessage(chatId, 'Aguarde um instante...');
+
+  getSharePrice(stockCode)
+    .then(function (cotacao) {
+      bot.sendMessage(
+        chatId,
+        `A cotação atual para ${stockCode}  é: ${cotacao}`
+      );
+    })
+    .catch(function () {
+      bot.sendMessage(chatId, 'Oppss, não encontramos o ativo! :/');
+    });
 });
 
 bot.onText(/\/comparar (.+) (.+)/, (msg, match) => {
